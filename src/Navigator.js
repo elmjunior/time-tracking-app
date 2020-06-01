4d7a72f0d79c50c39e9460d3b71cd03a8f6952a5
Edilson Junior <edilson.junior@kiper.com.br>
2020-06-01 16:35:56 -0300

    initial commit :rocket:

---
+++ src/Navigator.js
@@ -0,0 +1,10 @@
+import React from 'react';
+import {View} from 'react-native';
+
+// import { Container } from './styles';
+
+const Navigator = () => {
+  return <View />;
+};
+
+export default Navigator;
